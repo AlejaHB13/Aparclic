4239adc98bd161d065d19fdd1639537f1116fa75
Laura Beltran <lauras.beltranb@konradlorenz.edu.co>
2023-04-24 10:36:46 -0500

    Implementación clase registro empleado
    Se crea clase registroempleado.js en la carpeta models, donde se define nombre, cedula, usuario y contraseña

---
+++ src/models/registroempleado.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+
+const empleadoSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    cedula: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    usuario: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contraseña: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Empleado', empleadoSchema);
