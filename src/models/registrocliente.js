685f6ce2e0dd74396f8626d90e904072a57168f8
AleajaHB <alejandra.hernandezb@konradlorenz.edu.co>
2023-04-24 19:15:53 -0500

    Ruta de cliente
    Se agrega registrocliente.js en la carpeta de rutas

---
+++ src/models/registrocliente.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+
+const clienteSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    cedula: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    telefono: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Cliente', clienteSchema);
---
+++ src/routes/registrocliente.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const clienteSchema = require("../models/registrocliente"); //Nuevo cliente
