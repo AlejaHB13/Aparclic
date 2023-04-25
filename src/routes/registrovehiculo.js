f41b42f4986a41d2b8cd9985a93a1d64715a8eae
Andres Garcia <andreslapulgagarcia@gmail.com>
2023-04-25 11:30:55 -0500

    Registro nuevo vehículo
    Se agrega en el archivo registrovehiculo.js en la carpeta de rutas el código para realizar el endpoint para nuevo vehículo

--- src/routes/registrovehiculo.js
+++ src/routes/registrovehiculo.js
@@ -2,3 +2,11 @@ const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const vehiculoSchema = require("../models/registrovehiculo"); //Nuevo vehiculo
 
+//endpoint para Nuevo vehiculo
+router.post("/vehiculo", (req, res) => {
+    const vehiculo = vehiculoSchema(req.body);
+    vehiculo
+      .save()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
