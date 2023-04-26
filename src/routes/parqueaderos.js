10b094468703551350724e2cf2c3ea7371aeac25
Andres Garcia <andreslapulgagarcia@gmail.com>
2023-04-26 10:53:06 -0500

    Registro nuevo parqueadero
    Se agrega en el archivo parquederos.js en la carpeta de rutas el código para realizar el endpoint para nuevo parqueadero

--- src/routes/parqueaderos.js
+++ src/routes/parqueaderos.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const parqueaderoSchema = require("../models/parqueaderos"); //Nuevo parqueadero
+
+//endpoint para Nuevo parqueadero
+router.post("/parqueaderos", (req, res) => {
+    const parqueradero = parqueaderoSchema(req.body);
+    parqueadero
+      .save()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });   
+
+  
