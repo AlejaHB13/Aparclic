4a9876d774cdf23ac0c3870d95d4051ddf748c13
Andres Garcia <andreslapulgagarcia@gmail.com>
2023-04-26 10:09:28 -0500

    Registro nuevo usuario
    Se agrega en el archivo iniciosesion.js en la carpeta de rutas el código para realizar el endpoint para nuevo usuario

--- src/routes/iniciosesion.js
+++ src/routes/iniciosesion.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const sesionSchema = require("../models/iniciosesion"); //Nuevo usuario
+
+//endpoint para Nuevo usuario
+router.post("/sesion", (req, res) => {
+    const sesion = sesionSchema(req.body);
+    sesion
+      .save()
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
