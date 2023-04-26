67a82676cd03fa7f233e42c6a0f87cb3387bbf98
Andres Garcia <andreslapulgagarcia@gmail.com>
2023-04-26 10:37:29 -0500

    Parqueaderos
    Se crea clase parqueaderos.js en la carpeta models, donde se define pisos, cantidad de espacios y cantidad disponibles.

---
+++ src/models/parqueaderos.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+
+const parqueaderoSchema = mongoose.Schema({
+    pisos: {
+        type: int,
+        required: true
+    },
+    cantEspacios: {
+        type: int,
+        required: true,
+    },
+    cantDisponibles: {
+        type: int,
+        required: true,
+    },
+   
+});
+
+module.exports = mongoose.model('parqueadero', parqueaderoSchema);
