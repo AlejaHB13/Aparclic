46c3c17d0b593889caa986a57c3a7976d85056e5
Laura Beltran <lauras.beltranb@konradlorenz.edu.co>
2023-05-29 14:18:31 -0500

    Registro entrada vehiculo
    Se corrigen algunos errores

--- src/models/registroentradaveh.js
+++ src/models/registroentradaveh.js
@@ -11,10 +11,31 @@ const entradaSchema = mongoose.Schema({
         required: true
     },
     horaEntrada: {
-        type: String,
+        type: Date,
         required: true,
-    
-}
+        get: function () {
+            return formatTime(this.horaEntrada);
+        },
+        set: function (value) {
+            this.horaEntrada = parseTime(value);
+        }
+    }
 });
+
+function formatTime(time) {
+    // Formatea el objeto Date en una cadena de tiempo (HH:mm)
+    const hours = time.getHours().toString().padStart(2, '0');
+    const minutes = time.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
+
+function parseTime(value) {
+    // Analiza una cadena de tiempo (HH:mm) y devuelve un objeto Date
+    const [hours, minutes] = value.split(':');
+    const time = new Date();
+    time.setHours(parseInt(hours, 10));
+    time.setMinutes(parseInt(minutes, 10));
+    return time;
+}
 
 module.exports = mongoose.model('Entrada', entradaSchema);
